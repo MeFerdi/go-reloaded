a7e721a87abc8192b3c6beea929ee51b9aa91fb1
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-04-17 10:42:17 +0300

    fixed a bug in punctuation

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module student
 
-go 1.22.1
+go 1.23
--- student/commonpunctuation.go
+++ student/commonpunctuation.go
@@ -15,14 +15,28 @@ func FormatPunctuation(s string) string {
 				if string(TheString[0]) == ThePunctuation && string(TheString[len(TheString)-1]) != ThePunctuation && i != len(MyString)-1 {
 					MyString[i-1] = MyString[i-1] + ThePunctuation
 					MyString[i] = TheString[1:]
-				} else if string(TheString[0]) == ThePunctuation && i == len(MyString)-1 {
-					MyString[i-1] = MyString[i-1] + TheString
-					MyString = MyString[:len(MyString)-1]
-				} else if string(TheString[0]) == ThePunctuation && string(TheString[len(TheString)-1]) == ThePunctuation {
-					MyString[i-1] = MyString[i-1] + TheString
-					MyString = append(MyString[:i], MyString[i+1:]...)
-					i-- // Decrement i to account for the removed element
-				}
+				} 
+			}
+		}
+	}
+
+	for i := 0;i < len(MyString); i++ {
+		TheString := MyString[i]
+		for _, ThePunctuation := range MyPunctuations {
+			if string(TheString[0]) == ThePunctuation && i == len(MyString)-1 {
+				MyString[i-1] = MyString[i-1] + TheString
+				MyString = MyString[:len(MyString)-1]
+			}
+		}
+	}
+
+	for i := 0;i < len(MyString); i++ {
+		TheString := MyString[i]
+		for _, ThePunctuation := range MyPunctuations {
+			if string(TheString[0]) == ThePunctuation && string(TheString[len(TheString)-1]) == ThePunctuation {
+				MyString[i-1] = MyString[i-1] + TheString
+				MyString = append(MyString[:i], MyString[i+1:]...)
+				//i-- // Decrement i to account for the removed element
 			}
 		}
 	}
