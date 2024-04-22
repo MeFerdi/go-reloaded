ea8a464463dd99f4b67752c7cb9ee1f17e0256a2
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-04-22 15:10:46 +0300

    added config files, modified functions and fixed bugs

--- student/uniquepunctuation.go
+++ student/uniquepunctuation.go
@@ -31,29 +31,3 @@ func UniquePunctuation(s string) string {
 
 	return strings.Join(words, " ")
 }
-
-// 	words := strings.Fields(s)
-// 	count := 0
-
-// 	for i := 0; i < len(words); i++ {
-// 		if words[i] == "'" && count == 0 {
-
-// 			words[i+1] = "'" + words[i+1]
-
-// 			words = append(words[:i], words[i+1:]...)
-
-// 			count++
-// 		} else if words[i] == "'" && count != 0 {
-
-// 			words[i-1] = words[i-1] + "'"
-
-// 			words = append(words[:i], words[i+1:]...)
-
-// 			count--
-
-// 			i--
-// 		}
-// 	}
-
-// 	return strings.Join(words, " ")
-// }
