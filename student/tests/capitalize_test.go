4a6a61c1e6b8b6724e125b2eee2a102319d6d849
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-04-22 15:12:26 +0300

    removed result.txt file

--- student/tests/capitalize_test.go
+++ student/tests/capitalize_test.go
@@ -1,8 +1,6 @@
 package student
 
 import (
-	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -25,28 +23,28 @@ func TestCapitalize(t *testing.T) {
 	}
 }
 
-func Capitalize(s string) string {
-	words := strings.Split(s, " ")
+// func Capitalize(s string) string {
+// 	words := strings.Split(s, " ")
 
-	for i := 0; i < len(words); i++ {
-		if strings.Contains(words[i], "(cap") {
-			if strings.Contains(words[i], "(cap,") {
-				num, _ := strconv.Atoi(words[i+1][:len(words[i+1])-1])
-				if i-num >= 0 && i-num < len(words) {
-					for j := i - num; j < i; j++ {
-						if j >= 0 && j < len(words) {
-							words[j] = strings.ToUpper(string([]rune(words[j])[0])) + strings.ToLower(words[j][1:])
-						}
-					}
-					words = append(words[:i], words[i+2:]...)
-				}
-			} else {
-				if i >= 0 && i < len(words) {
-					words[i-1] = strings.ToUpper(string([]rune(words[i-1])[0])) + strings.ToLower(words[i-1][1:])
-					words = append(words[:i], words[i+1:]...)
-				}
-			}
-		}
-	}
-	return strings.Join(words, " ")
-}
+// 	for i := 0; i < len(words); i++ {
+// 		if strings.Contains(words[i], "(cap") {
+// 			if strings.Contains(words[i], "(cap,") {
+// 				num, _ := strconv.Atoi(words[i+1][:len(words[i+1])-1])
+// 				if i-num >= 0 && i-num < len(words) {
+// 					for j := i - num; j < i; j++ {
+// 						if j >= 0 && j < len(words) {
+// 							words[j] = strings.ToUpper(string([]rune(words[j])[0])) + strings.ToLower(words[j][1:])
+// 						}
+// 					}
+// 					words = append(words[:i], words[i+2:]...)
+// 				}
+// 			} else {
+// 				if i >= 0 && i < len(words) {
+// 					words[i-1] = strings.ToUpper(string([]rune(words[i-1])[0])) + strings.ToLower(words[i-1][1:])
+// 					words = append(words[:i], words[i+1:]...)
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return strings.Join(words, " ")
+// }
--- student/tests/hextodec_test.go
+++ student/tests/hextodec_test.go
@@ -1,9 +1,6 @@
 package student
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -26,14 +23,14 @@ func TestHexToDecimal(t *testing.T) {
 	}
 }
 
-func HexToDecimal(s string) string {
-	words := strings.Fields(s)
-	for i := 0; i < len(words); i++ {
-		if words[i] == "(hex)" && i > 0 {
-			hexValue, _ := strconv.ParseInt(words[i-1], 16, 64)
-			words[i-1] = fmt.Sprintf("%d", hexValue)
-			words = append(words[:i], words[i+1:]...)
-		}
-	}
-	return strings.Join(words, " ")
-}
+// func HexToDecimal(s string) string {
+// 	words := strings.Fields(s)
+// 	for i := 0; i < len(words); i++ {
+// 		if words[i] == "(hex)" && i > 0 {
+// 			hexValue, _ := strconv.ParseInt(words[i-1], 16, 64)
+// 			words[i-1] = fmt.Sprintf("%d", hexValue)
+// 			words = append(words[:i], words[i+1:]...)
+// 		}
+// 	}
+// 	return strings.Join(words, " ")
+// }
